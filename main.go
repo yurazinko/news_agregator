b54f659e66198d3b969b5b470bfba8acb7288d26
Yurii Zinko <yurii.zinko@vertalab.com>
2018-04-10 11:15:22 +0300

    Refactor structure

--- main.go
+++ main.go
@@ -8,26 +8,26 @@ import (
 )
 
 type SitemapIndex struct {
-  Locations []Location `xml:"sitemap"`
+  Locations []string `xml:"sitemap>loc"`
 }
 
-type Location struct {
-  Loc string `xml:"loc"`
-}
-
-func (l Location) String() string {
-  return fmt.Sprintf(l.Loc)
-}
+type News struct {
+  Titles []string `xml:"url>news>title"`
+  Keywords []string `xml:"url>news>Keywords"`
+  Locations []string `xml:"url>loc"`
+  }
 
 func main() {
+    var s SitemapIndex
+    var n News
+
     resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
     bytes, _ := ioutil.ReadAll(resp.Body)
-    stringBody := string(bytes)
-    fmt.Println(stringBody)
-    resp.Body.Close()
-
-    var s SitemapIndex
     xml.Unmarshal(bytes, &s)
 
-    fmt.Println(s.Locations)
+    for _, Location := range s.Locations {
+        resp, _ := http.Get(Location)
+        bytes, _ := ioutil.ReadAll(resp.Body)
+        xml.Unmarshal(bytes, &n)
+    }
 }
