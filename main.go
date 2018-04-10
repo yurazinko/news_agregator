da4e6a0519d652b9dffbdcb20054f85b34b38146
Yurii Zinko <yurii.zinko@vertalab.com>
2018-04-10 10:02:22 +0300

    Add XML parser

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "io/ioutil"
+    "encoding/xml"
+)
+
+type SitemapIndex struct {
+  Locations []Location `xml:"sitemap"`
+}
+
+type Location struct {
+  Loc string `xml:"loc"`
+}
+
+func (l Location) String() string {
+  return fmt.Sprintf(l.Loc)
+}
+
+func main() {
+    resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+    bytes, _ := ioutil.ReadAll(resp.Body)
+    stringBody := string(bytes)
+    fmt.Println(stringBody)
+    resp.Body.Close()
+
+    var s SitemapIndex
+    xml.Unmarshal(bytes, &s)
+
+    fmt.Println(s.Locations)
+}
