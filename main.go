e757313b5c212266b1e1df8bf449aaea216de93f
Yurii Zinko <yurii.zinko@vertalab.com>
2018-04-13 10:04:08 +0300

    Add news map

--- main.go
+++ main.go
@@ -13,14 +13,19 @@ type SitemapIndex struct {
 
 type News struct {
   Titles []string `xml:"url>news>title"`
-  Keywords []string `xml:"url>news>Keywords"`
+  Keywords []string `xml:"url>news>keywords"`
   Locations []string `xml:"url>loc"`
   }
+
+type NewsMap struct {
+  Keyword string
+  Location string
+}
 
 func main() {
     var s SitemapIndex
     var n News
-
+    newsMap := make(map[string]NewsMap)
     resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
     bytes, _ := ioutil.ReadAll(resp.Body)
     xml.Unmarshal(bytes, &s)
@@ -29,5 +34,14 @@ func main() {
         resp, _ := http.Get(Location)
         bytes, _ := ioutil.ReadAll(resp.Body)
         xml.Unmarshal(bytes, &n)
+        for i, _ :=range n.Titles {
+            newsMap[n.Titles[i]] = NewsMap{n.Keywords[i], n.Locations[i]}
+        }
+    }
+
+    for i, data := range newsMap {
+      fmt.Println("\n", i)
+      fmt.Println("\n", data.Keyword)
+      fmt.Println("\n", data.Location)
     }
 }
